9beedbb3f40f6623bb8f72617bd89fe6b4396aae
Rubén <trinitymou9@gmail.com>
2026-05-13 17:11:02 +0200

    Bump version to 1.4.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,9 +3,31 @@ game 'gta5'
 
 author 'Mads'
 description 'Telescopes'
-version '1.4.1'
+version '1.4.2'
 
-client_scripts {
-	'config.lua',
-	'client.lua'
+ox_lib 'locale'
+
+shared_scripts {
+    '@ox_lib/init.lua',
 }
+
+client_script {
+    'client/main.lua',
+}
+
+server_script {
+    'server/main.lua',
+}
+
+files {
+    'locales/*.json',
+    'config/client.lua',
+    'config/server.lua',
+}
+
+dependencies {
+    'ox_lib'
+}
+
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
